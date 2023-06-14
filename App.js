ee81e44b4c4b9a86922dd41dc2dc37109f4f50de
Nitinnihal <nnjha1999@gmail.com>
2023-06-15 01:48:26 +0530

    Ep-03 part-04

--- App.js
+++ App.js
@@ -1,99 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
+// React functional components
 
-
-
-// const heading = React.createElement("h1" , {id:"heading"} , "hello world from React.");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-
-
-// 1/*
-//    <div id = "parent">
-//         <div id = "child">
-//             <h1>
-//             I'm inside h1 tag.
-//             </h1>
-//         </div>
-//     </div>
-
-
-// */
-
-// // const parent = React.createElement("div" , {id:"parent"} , 
-// //     React.createElement("div" , {id:"child"} , 
-// //         React.createElement("h1" , {} , "I'm inside h1 tag.")
-// //     )
-// // )
-// // console.log(parent);
-// // const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// // root.render(parent);
-
-
-
-// /*2
-//     <div id = "parent">
-//         <div id = "child">
-//             <h1>I'm inside h1 tag!</h1>
-//             <h2>I'm sibling of h1 tag</h2>
-//         </div>
-//     </div>
-
-// */
-
-// const parent = React.createElement("div" , {id:"parent"} , 
-//     React.createElement("div" , {id:"child"} , 
-//         [
-//             React.createElement("h1" , {} , "I'm  inside h1 tag !"),
-//             React.createElement("h2" , {} , "I'm sibling of h1 tag !!")
-//         ]
-//     )
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(parent);
-
-
-
-
-/*3
-
-    <div id = "parent">
-       <div id = "child1">
-            <h1>I'm inside h1 tag!</h1>
-             <h2>I'm sibling of h1 tag</h2>
-         </div>
-         <div id = "child2">
-            <h1>I'm inside h1 tag!</h1>
-             <h2>I'm sibling of h1 tag</h2>
-         </div>
-     </div>
-*/ 
-
-
-const parent = React.createElement("div" , {id:"parent"} , 
-
-    [
-        React.createElement("div" , {id:"child1"} , 
-            [
-                React.createElement("h1" , {} , "this is nameste react !") , 
-                React.createElement("h2" , {} , "I'm sibling of h1 tag!")
-            ]
-        ) , 
-        React.createElement("div" , {id:"child2"} , 
-            [
-                React.createElement("h1" , {} , "I'm inside h1 tag!") , 
-                React.createElement("h2" , {} , "I'm sibling of h1 tag!")
-
-            ]
-        )
-    ]
+const Title = () => (
+    <div id = "title">
+        <h1> Nameste React</h1>
+    </div>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const Heading = () => (
+<div id = "container">
+    <Title />
+    <h1 className = "heading">Nameste react from Functional component 🚀. </h1>
+</div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading />);
