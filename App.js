8b9db7f9a41c05970954fed1ec716bfd02682c6b
Nitinnihal <nnjha1999@gmail.com>
2023-06-15 17:31:55 +0530

    Ep-03

--- App.js
+++ App.js
@@ -3,18 +3,35 @@ import ReactDOM from "react-dom/client";
 
 // React functional components
 
+// const Title = function(){
+//     return(
+//         <h1>hello Nameste React🚀</h1>
+//     )
+// };
+
+const elem = (
+    <sapn>(by akshay saini)</sapn>
+);
+
 const Title = () => (
     <div id = "title">
-        <h1> Nameste React</h1>
+        <h1>{elem}Nameste React</h1>
     </div>
 );
 
 
 const Heading = () => (
+    <>
+     {/* <React.Fragment> */}
 <div id = "container">
+    <Title></Title>
     <Title />
+   { Title() }
     <h1 className = "heading">Nameste react from Functional component 🚀. </h1>
 </div>
+<div id = "container-2"></div>
+      {/* </React.Fragment> */}
+</>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Heading />);
